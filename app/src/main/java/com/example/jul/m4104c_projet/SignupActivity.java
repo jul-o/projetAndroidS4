58b505b798f4bbfb8207c9f873fa3b27e9792a9d
jul <jules.westersang@gmail.com>
2018-03-10 16:14:46 +0100

    first true commit

---
+++ app/src/main/java/com/example/jul/m4104c_projet/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.jul.m4104c_projet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
